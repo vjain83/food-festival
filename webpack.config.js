f3eedcc5a7de7c44fec8e5026085419938594c71
Varsha Jain <csvarshajain@gmail.com>
2021-12-23 15:30:09 -0500

    added wepack and file-loader to reduce the file size

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,52 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry:
+    {
+        app: './assets/js/script.js',
+        event: './assets/js/event.js',
+        schedule: './assets/js/schedule.js',
+        ticket: './assets/js/ticket'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: __dirname + "/dist",
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function (url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 
